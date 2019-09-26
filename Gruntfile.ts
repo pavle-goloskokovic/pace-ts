8ef599e6f8f4669d7e01b206934e1d246ada7c69
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2019-09-26 13:43:27 +0200

    converted .coffee source files to typescript using decaffeinate

---
+++ Gruntfile.ts
@@ -0,0 +1,91 @@
+/*
+ * decaffeinate suggestions:
+ * DS101: Remove unnecessary use of Array.from
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+*/
+  const Path = require('path');
+const fs = require('fs');
+
+const ThemeUtils = require('./docs/lib/themes.coffee');
+
+const themeColors = {
+  black:  '#000000',
+  white:  '#ffffff',
+  silver: '#d6d6d6',
+  red:    '#ee3148',
+  orange: '#eb7a55',
+  yellow: '#fcd25a',
+  green:  '#22df80',
+  blue:   '#2299dd',
+  pink:   '#e90f92',
+  purple: '#7c60e0'
+};
+
+module.exports = function(grunt) {
+  grunt.registerTask('themes', 'Compile the pace theme files', function() {
+  const done = this.async();
+
+const options = grunt.config('themes');
+
+return grunt.file.glob(options.src, function(err, files) {
+  for (let colorName in themeColors) {
+    const color = themeColors[colorName];
+for (let file of Array.from(files)) {
+  let body = ThemeUtils.compileTheme(fs.readFileSync(file).toString(), {color});
+
+body = "/* This is a compiled file, you should be editing the file in the templates directory */\n" + body;
+
+let name = Path.basename(file);
+name = name.replace('.tmpl', '');
+const path = Path.join(options.dest, colorName, name);
+
+fs.writeFileSync(path, body);
+}
+}
+
+return done();
+});
+});
+
+grunt.initConfig({
+  pkg: grunt.file.readJSON("package.json"),
+  coffee: {
+    compile: {
+      files: {
+        'pace.js': 'pace.coffee',
+        'docs/lib/themes.js': 'docs/lib/themes.coffee'
+      }
+    }
+  },
+
+  watch: {
+    coffee: {
+      files: ['pace.coffee', 'docs/lib/themes.coffee', 'templates/*'],
+      tasks: ["coffee", "uglify", "themes"]
+    }
+  },
+
+  uglify: {
+    options: {
+      banner: "/*! <%= pkg.name %> <%= pkg.version %> */\n"
+    },
+
+    dist: {
+      src: 'pace.js',
+      dest: 'pace.min.js'
+    }
+  },
+
+  themes: {
+    src: 'templates/*.tmpl.css',
+    dest: 'themes'
+  }
+});
+
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-coffee');
+
+return grunt.registerTask('default', ['coffee', 'uglify', 'themes']);
+};
---
+++ docs/lib/themes.ts
@@ -0,0 +1,43 @@
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * DS207: Consider shorter variations of null checks
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+let Color, vm;
+if (typeof module !== 'undefined' && module !== null) {
+    vm = require('vm');
+
+    // Used by the eval'd code
+    Color = require('color');
+}
+
+const loadTheme = (name, cb) => $.ajax({
+    url: `/pace/templates/pace-theme-${ name }.tmpl.css`,
+    success: cb
+});
+
+const compileTheme = function(body, args) {
+    if (args == null) { args = {}; }
+    return body.replace(/`([\s\S]*?)`/gm, function(match, code) {
+        let val;
+        if (typeof module !== 'undefined' && module !== null) {
+            val = vm.runInNewContext(code, {args, Color});
+        } else {
+            // It matters that args is in the context
+            ({
+                Color
+            } = window);
+            val = eval(code);
+        }
+
+        return val;
+    });
+};
+
+if (typeof module !== 'undefined' && module !== null) {
+    module.exports = {compileTheme};
+} else {
+    window.loadTheme = loadTheme;
+    window.compileTheme = compileTheme;
+}
